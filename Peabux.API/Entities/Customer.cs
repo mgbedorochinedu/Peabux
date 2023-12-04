f0f41a0aad6c69ae06a341b2e18d9c2339da47dc
Chinedu Mgbedoro <Chinedu.Mgbedoro@sterling.ng>
2023-12-04 16:18:51 +0100

    Add customer entity

--- Peabux.API/Entities/Customer.cs
+++ Peabux.API/Entities/Customer.cs
@@ -1,7 +1,15 @@
 ﻿namespace Peabux.API.Entities
 {
-    public class Customer
+    public class Customer : BaseEntity
     {
- 
+        public int CustomerId { get; set; }
+        public string? NationalID { get; set; }
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public DateTime? DOB { get; set; }
+        public string? CustomerNumber { get; set; }
+        public string? TransactionHistory { get; set; }
+
+
     }
 }
