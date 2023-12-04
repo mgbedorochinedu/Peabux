213abe1689ec330f222b496bb6b2e9a22f8492ee
Chinedu Mgbedoro <Chinedu.Mgbedoro@sterling.ng>
2023-12-04 23:02:17 +0100

    Add base response class

--- Peabux.API/ServiceResponse/BaseResponse.cs
+++ Peabux.API/ServiceResponse/BaseResponse.cs
@@ -2,6 +2,15 @@
 {
     public class BaseResponse
     {
-      
+        public bool Success { get; set; }
+        public object Data { get; set; }
+        public string Message { get; set; }
+
+        public BaseResponse(bool success, object data, string message)
+        {
+            Success = success;
+            Data = data;
+            Message = message;
+        }
     }
 }
