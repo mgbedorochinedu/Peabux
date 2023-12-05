10745809508ee540553021cc920a86feee29cd3e
Chinedu Mgbedoro <Chinedu.Mgbedoro@sterling.ng>
2023-12-05 09:14:07 +0100

    Add a create customer controller

---
+++ Peabux.API/Controllers/CustomerController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Peabux.API.Models;
+using Peabux.API.Services.CustomerService;
+
+namespace Peabux.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICustomerService _customerService;
+
+        public CustomerController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> CreateCustomer([FromBody] CreateCustomerModel model)
+        {          
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+                var response = await _customerService.CreateCustomer(model);
+                return Ok(response);                      
+        }
+    }
+}
