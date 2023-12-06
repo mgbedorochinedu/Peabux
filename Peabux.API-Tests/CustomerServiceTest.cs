6cfef0a7e8471cc8612f5e786c13aa49be72f3f9
Chinedu Mgbedoro <Chinedu.Mgbedoro@sterling.ng>
2023-12-06 13:08:19 +0100

    set up test database

--- Peabux.API-Tests/CustomerServiceTest.cs
+++ Peabux.API-Tests/CustomerServiceTest.cs
@@ -1,18 +1,24 @@
+using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
+using Peabux.API.Data;
 
 namespace Peabux.API_Tests
 {
     public class CustomerServiceTest
     {
-        [SetUp]
+        private static DbContextOptions<AppDbContext> dbContextOptions = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "PeabuxDbTest")
+            .Options;
+
+        AppDbContext dbContext;
+
+
+        [OneTimeSetUp]
         public void Setup()
         {
+            dbContext = new AppDbContext(dbContextOptions);
+            dbContext.Database.EnsureCreated();
         }
 
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
     }
 }
