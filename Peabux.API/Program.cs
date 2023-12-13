bacead010af42b54f164868bd804f823dc46b960
Chinedu Mgbedoro <Chinedu.Mgbedoro@sterling.ng>
2023-12-14 00:33:16 +0100

    Show conrtoller API Docs

--- Peabux.API/Program.cs
+++ Peabux.API/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using Peabux.API.Data;
 using Peabux.API.Services.CustomerService;
 using Peabux.API.Services.MerchantService;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,7 +18,11 @@ builder.Services.AddScoped<IMerchantService, MerchantService>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory,
+        $"{Assembly.GetExecutingAssembly().GetName().Name}.xml"));
+});
 
 var app = builder.Build();
 
